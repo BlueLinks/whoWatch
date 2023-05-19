25c27aaa5f921651ec624ea9462909fe0232d5fe
Scott Brown <scott_m_brown@mac.com>
2023-05-19 23:45:51 +0100

    Closes #10, implemented countdown timer on for current episode

--- whoWatch/currentEpisodeView.swift
+++ whoWatch/currentEpisodeView.swift
@@ -15,6 +15,14 @@ struct currentEpisodeView: View {
     var logo: Image
     var function: () -> Void
     
+    @State private var timeRemaining = 86400.00 // default value 24hrs in seconds
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var formattedTimeRemaining = ""
+    
+    @State var workoutDateRange = Date()...Date().addingTimeInterval(5*60)
+    
+    let formatter = DateComponentsFormatter()
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20, style: .continuous)
@@ -34,6 +42,7 @@ struct currentEpisodeView: View {
                         .multilineTextAlignment(.center)
                 }
                 if title != "Watch Now!"{
+                    // This is not the current episode
                     Button(){
                         self.function()
                     } label: {
@@ -44,24 +53,46 @@ struct currentEpisodeView: View {
                             .clipShape(Capsule())
                     }
                     .padding()
+                } else {
+                    // This is the current episode, show timer
+                    VStack{
+                        Text("Time left to watch...")
+                            .font(.headline)
+                        Text("\(formattedTimeRemaining)")
+                    }
+                    .frame(width: 250, height: 75)
+                    .background(.black.opacity(0.5))
+                                .clipShape(Capsule())
                 }
             }.padding()
         }
+        .onAppear(){
+            timeRemaining = currentEpisode.endTime.timeIntervalSince(Date())
+            workoutDateRange = currentEpisode.startTime...currentEpisode.endTime
+        }
+        .onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+                formatter.unitsStyle = .short
+                formattedTimeRemaining = formatter.string(from: TimeInterval(timeRemaining))!
+            }
+        }
         .edgesIgnoringSafeArea(.all)
     }
+    
 }
 
 struct currentEpisodeView_Previews: PreviewProvider {
     static var previews: some View {
         let episodeLib = episodeLibrary(episodes: Bundle.main.decode("doccyWho.json"))
         
-        let controller = watchCardViews(mainEpisode: .constant(episodeLib.episodes[1]),
+        let controller = watchCardViews(mainEpisode: .constant(episodeLib.episodes[0]),
                                         episodeLib: episodeLib)
         
-        let currentEp = episodeLib.episodes[5]
+        let currentEp = episodeLib.whatToWatch()
         ZStack{
             controller.getBackgroundColor(ep: currentEp)
-            currentEpisodeView(title: "Next", currentEpisode: currentEp, backgroundColor: controller.getBackgroundColor(ep: currentEp), logo: controller.getLogo(currentEp: currentEp), function: { print("Going back to current") })
+            currentEpisodeView(title: "Watch Now!", currentEpisode: currentEp, backgroundColor: controller.getBackgroundColor(ep: currentEp), logo: controller.getLogo(currentEp: currentEp), function: { print("Going back to current") })
         }.preferredColorScheme(.dark)
     }
 }
